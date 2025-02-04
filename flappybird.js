aa2ae651e9477a37f801a642f4879a653e9e53cc
bossmoon229 <alinaignatich@gmail.com>
2025-02-04 23:22:08 +0200

    place bg and bird

--- flappybird.js
+++ flappybird.js
@@ -5,9 +5,37 @@ let boardWidth = 360;
 let boardHeight = 640;
 let context;
 
-window.onload = function() {
-    board = document.getElementById('board');
-    board.height = boardHeight;
-    board.width = boardWidth;
-    context = board.getContext('2d'); // used for drawing board
-}
+// ! bird
+
+let birdWidth = 34;
+let birdHeight = 24;
+let birdX = boardWidth / 8;
+let birdY = boardHeight / 2;
+let birdImg;
+
+let bird = {
+  x: birdX,
+  y: birdY,
+  width: birdWidth,
+  height: birdHeight,
+};
+
+window.onload = function () {
+  board = document.getElementById("board");
+  board.height = boardHeight;
+  board.width = boardWidth;
+  context = board.getContext("2d"); // used for drawing board
+
+  //place bird
+
+//   context.fillstyle = "red";
+//   context.fillRect(bird.x, bird.y, bird.width, bird.height);
+
+  // load img
+
+  birdImg = new Image();
+  birdImg.src = "./flappybird.png";
+  birdImg.onload = function () {
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+  };
+};
